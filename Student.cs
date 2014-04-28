951305e5ee7c4bc2b015cb63562abdba2706b8ba
lukas <lukas.cech12@gmail.com>
2014-04-28 13:44:08 +0200

    Added basic ViewModel.

--- Student.cs
+++ Student.cs
@@ -8,5 +8,27 @@ namespace LetsDoThis
 {
     class Student
     {
+        private string _name;
+        private string _address;
+        private int _age;
+
+
+        public string Name
+        {
+            get { return _name; }
+            set{_name = value};
+        }
+
+        public string Address
+        {
+            get{return _address} 
+            set { _address = value; }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
     }
 }
---
+++ StudentViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using LetsDoThis.Annotations;
+
+namespace LetsDoThis
+{
+    class StudentViewModel : INotifyPropertyChanged
+    {
+        private readonly string _currentStudentName = "CurrentStudent";
+        private readonly string _listStudents = "Students";
+
+
+        private Student _currentStudent;
+        private List<Student> _Students;
+
+
+        public Student CurrentStudent
+        {
+            get { return _currentStudent; }
+            set
+            {
+                if (value != _currentStudent)
+                {
+                    _currentStudent = value;
+                    OnPropertyChanged(_currentStudentName);
+                }
+            } 
+        }
+
+        public ObservableCollection<Student> Students
+        {
+            get
+            {
+                ObservableCollection<Student> students = new ObservableCollection<Student>();
+                foreach (var Student in _Students)
+                {
+                    students.Add(Student);
+                }
+                return students;
+            }
+        }
+
+
+
+        #region InotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+    }
+}
